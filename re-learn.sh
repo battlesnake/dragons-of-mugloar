475dbf95b6625dc937675f9ea054ffdc7645fc3c
Mark K Cowan <mark@battlesnake.co.uk>
2019-08-04 19:14:42 +0300

    ?: removed re-learn.sh

--- re-learn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-./mugcollect -o training.dat -p 100
-
-./muglearn -i training.dat -o feature_score.dat
-
-./mugomatic -i feature_score.dat -o training.dat -s scores.dat -p 100
